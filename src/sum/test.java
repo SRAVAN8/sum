6178dfd1af3cca97a6c031587d228a38ee0ccae7
R Sravan Kumar Reddy <R Sravan Kumar Reddy@DESKTOP-BB6LMPN>
2019-07-08 23:32:30 +0530

    First Commit

---
+++ src/sum/test.java
@@ -0,0 +1,14 @@
+package sum;
+
+public class test {
+
+	
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("this is a:"+a);
+		System.out.println("this is b:"+b);
+		System.out.println("this is sum of "+a+"and"+b+"="+c);
+	}
+}
